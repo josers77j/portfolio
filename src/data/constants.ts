5917a80a75f1aef0aed444787269f2ae1eb18d1b
josers77j <josers77.1@gmail.com>
2025-11-29 22:09:28 -0600

    fix: image moved to public

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,65 +1,65 @@
 export const stackCollections = [
     {"name":"TypeScript",
-    "image":"../../src/utils/icons/typescript/typescript-original.svg"
+    "image":"/icons/typescript/typescript-original.svg"
     },
     {
       "name": "JavaScript",
-      "image": "../../src/utils/icons/javascript/javascript-original.svg"
+      "image": "/icons/javascript/javascript-original.svg"
     },
     {
       "name": "PHP",
-      "image": "../../src/utils/icons/php/php-original.svg"
+      "image": "/icons/php/php-original.svg"
     },
     {
       "name": "JAVA",
-      "image": "../../src/utils/icons/java/java-original.svg"
+      "image": "/icons/java/java-original.svg"
     },
     {
       "name": "ExpressJS",
-      "image": "../../src/utils/icons/express/express-original.svg"
+      "image": "/icons/express/express-original.svg"
     },
     {
       "name": "NestJS",
-      "image": "../../src/utils/icons/nestjs/nestjs-original.svg"
+      "image": "/icons/nestjs/nestjs-original.svg"
     },
     {
       "name": "Laravel",
-      "image": "../../src/utils/icons/laravel/laravel-original.svg"
+      "image": "/icons/laravel/laravel-original.svg"
     },
     {
       "name": "Spring Boot",
-      "image": "../../src/utils/icons/spring/spring-original.svg"
+      "image": "/icons/spring/spring-original.svg"
     },
     {
       "name": "NodeJS",
-      "image": "../../src/utils/icons/nodejs/nodejs-original.svg"
+      "image": "/icons/nodejs/nodejs-original.svg"
     },
     {
       "name": "GitHub",
-      "image": "../../src/utils/icons/github/github-original.svg"
+      "image": "/icons/github/github-original.svg"
     },
     {
       "name": "Docker",
-      "image": "../../src/utils/icons/docker/docker-original.svg"
+      "image": "/icons/docker/docker-original.svg"
     },
     {
       "name": "MySQL",
-      "image": "../../src/utils/icons/mysql/mysql-original.svg"
+      "image": "/icons/mysql/mysql-original.svg"
     },
     {
       "name": "MongoDB",
-      "image": "../../src/utils/icons/mongodb/mongodb-original.svg"
+      "image": "/icons/mongodb/mongodb-original.svg"
     },
   ]
 
   export const projectCollections = [
     {"title":"Sistema de Gestión de Inventario",
-    "image":"../../src/img/inventoryManagement.png",
+    "image":"/img/inventoryManagement.png",
     "description":"Descripcion: Sistema de gestión de inventario para un instituto que imparte cursos de formación profesional. Permite llevar un control de los productos, proveedores, compras y ventas.",
     "techs":"Tecnologias: PHP, AJAX, MySQL, MVC, HTML, CSS, JavaScript, jQuery, Bootstrap, Github",
     },
     {"title":"Sistema de Gestión de Inventario",
-      "image":"../../src/img/inventoryManagement.png",
+      "image":"/img/inventoryManagement.png",
       "description":"Descripcion: Sistema .",
       "techs":"Tecnologias: PHP, AJAX, MySQL, MVC, HTML, CSS, JavaScript, jQuery, Bootstrap, Github",
       },
